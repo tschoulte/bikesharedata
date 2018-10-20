9274876a1970f7069dad0ba63c44ca3fcd497b9f
tschoulte <tschoulte@gmail.com>
2018-10-20 11:10:10 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import csv
 from math import sin, cos, sqrt, atan2, radians
+from subprocess import Popen
+import sys
 
 #initialization of the indices of the excel columns
 tripID, duration = 0, 1
@@ -12,7 +14,7 @@ tripCat, passType = 12, 13
 startLatLong, endLatLong = 14, 15
 
 #open csv file
-f = open('data_backup.csv')
+f = open('data_short.csv')
 csv_f = csv.reader(f)
 
 #import csv file into a list of lists. Remove the header row
@@ -21,7 +23,39 @@ for row in csv_f:
   data.append(row)
 data.pop(0)
 
-#2: Requesting most popular start and stop locations
+#!!!!############### Main Menu ##########################!!!!#
+#Function: Main Menu
+def main_menu():
+    #print the main command options
+    print("0. Exit Program")
+    print("1. N/A")
+    print("2. Request most popular start locations")
+    print("3. Request most popular stop locations")
+    print("4. What is the average distance traveled")
+    print("5. Analyze ridership patterns (e.g. PassType, Duration)")
+    print("6. Which locations require bike transport to maintain bike levels?")
+    print("7. What is the breakdown of Trip Route Category-Passholder type combinations?")
+    
+    #call the input dialog box
+    message = "What data do you desire?" #prompt 
+    x = take_input(6, message, 0)
+
+    #Cases: decide on the respective function based on integer input
+    command = None
+    command = sys.exit() if x == 0 else command
+    command = sys.exit() if x == 1 else command
+    command = most_popular_stations("Start") if x == 2 else command
+    command = most_popular_stations("Stop") if x == 3 else command
+    command = average_distance_traveled() if x == 4 else command
+    command = sys.exit() if x == 5 else command
+    command = sys.exit() if x == 6 else command
+    command = sys.exit() if x == 7 else command
+    
+    command #call the decided function
+
+#!!!!################## Primary Functions #######################!!!!#
+
+#Function 2: Request most popular start or stop locations
 def most_popular_stations(start_stop):
     popular_loc = {} #create dictionary of most popular start or stop location IDs
 
@@ -50,19 +84,20 @@ def most_popular_stations(start_stop):
     desired_quantity = take_input(max_ans, message, min_ans)
 
     #write the desired quantity of data to "output_filename"
-    output_filename = 'Popular ' + start_stop + ' Locations.csv'
+    output_filename = 'Popular ' + start_stop + ' Locations.csv' #default filename
+    output_filename = choose_filename(output_filename)           #allows user to specify unique filename
     columnheaders = ["Popular " + start_stop + " Locations", "Occurances"]
     data_list = popular_loc
     return_string = output_filename + " now has the " + str(desired_quantity) + " most popular " + start_stop.lower() + " locations."
     output_data(output_filename, columnheaders, data_list, return_string, desired_quantity)
 
-#3: Find the average distance traveled
+#Function 3: Find the average distance traveled
 def average_distance_traveled():
     R = 6373.0 # approximate radius of earth in km
-    total_distance = 0
-    num_rows = 0
-    round_trip_counter = 0
-    blank_counter = 0
+    total_distance = 0 #sum of all valid distances
+    num_rows = 0 #used for dividing sum of distance for average distance. Excludes round_trip_counter and blank_counter
+    round_trip_counter = 0 #count number of round trips (because distance for a round trip is 0km)
+    blank_counter = 0 #count number of empty-rows for start or stop location. These shouldn't be included
 
     for row in data: #for each row in the dataset
         if row[tripCat] == "One Way": #only include one-ways, as round-trips have a distance traveled of 0
@@ -90,7 +125,7 @@ def average_distance_traveled():
         else: #if the trip is a round trip, increment round_trip_counter
             round_trip_counter += 1
     
-    #parse return messages
+    #parse messages we will return
     return_message2 = str(round_trip_counter)
     return_message3 = str(blank_counter)
     try:
@@ -100,12 +135,15 @@ def average_distance_traveled():
         return_message1 = "All trips were round trips, no average"
 
     #write the desired quantity of data to "output_filename"
-    output_filename = 'Average Distance Traveled.csv'
+    output_filename = 'Average Distance Traveled.csv' #default filename
+    output_filename = choose_filename(output_filename) #allows user to specify unique filename
     columnheaders = ["Average Distance Traveled (km)", "# of Round Trips Excluded", "Number of Blank Entries Excluded"]
     data_list = [[return_message1, return_message2, return_message3]]
     return_string = output_filename + " now has the average distance traveled stored!"
     desired_quantity = len(data_list)
     output_data(output_filename, columnheaders, data_list, return_string, desired_quantity)
+
+#!!!!################## Secondary Input/Parsing Functions #######################!!!!#
 
 #Dialog box maker, with checks in place
 def take_input(max_ans, message, min_ans=0):
@@ -124,6 +162,25 @@ def take_input(max_ans, message, min_ans=0):
         ans = take_input(max_ans, message, min_ans)
     return ans
 
+#File name chooser. Allows for custom filenames or to accept the default filename
+def choose_filename(output_filename):
+    #Take input of file name
+    ans = input("Choose filename (including '.csv'). Press 'Enter' to accept default filename [" + output_filename + "]: ")
+
+    #if the user chose the default filename by pressing enter, assign the default filename to ans
+    if ans == "":
+        ans = output_filename
+
+    #checks if the choosen filename meets the criteria (.csv extension)     
+    if '.csv' not in ans:
+        print("Invalid entry! Needs '.csv' file extension!")
+        ans = choose_filename(output_filename)
+
+    return ans
+
+#!!!!################## Data Export Functions #######################!!!!#
+
+#Exports the data to .csv files, provided 5 arguments
 def output_data(output_filename, columnheaders, data_list, return_string, desired_quantity = 0):
     #write the desired locations to "output_filename"
     i = 0
@@ -145,15 +202,17 @@ def output_data(output_filename, columnheaders, data_list, return_string, desire
 
 
 #2a: Run command to find the most popular start locations
-most_popular_stations("Start")
+#most_popular_stations("Start")
 
 #2b: run command to find the most popular stop locations
-most_popular_stations("Stop")
+#most_popular_stations("Stop")
 
 #3: run command to find the average distance traveled
-average_distance_traveled()
+#average_distance_traveled()
+#p = Popen('Average Distance Traveled.csv', shell=True)
 
 #4: Run command to find how many riders regularly commute via bikeshare?
 # Calculate Monthly or Annual passes
 #Flex pass: "Best if you expect to take two to four trips per month"
 #Monthly pass: "Best if you expect to take five or more trips per month"
+main_menu()
